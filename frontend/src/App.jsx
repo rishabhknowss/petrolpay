d0546494c077bdb372b9f75963fb0b0787070e1f
kapoorsaumitra <saumitrakapoor24@gmail.com>
2024-03-30 00:07:43 +0530

    feat: signup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,15 @@
 import "./App.css";
 import { Signin } from "./components/Signin";
 import { BrowserRouter , Routes, Route } from "react-router-dom";
+import { Signup } from "./components/Signup";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/signup" element={<Signup />} /> */}
           <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
           {/* <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/send" element={<SendMoney />} /> */}
         </Routes>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,26 +1,23 @@
-import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
 
-export const Signin = () => {
+export const Signup = () => {
   return (
     <div className="bg-blue-300 h-screen flex justify-center">
       <div className="flex flex-col justify-center">
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
-          <Heading label={"Sign in"} />
-          <SubHeading label={"Enter your credentials to access your PetrolPay account"} />
+          <Heading label={"Sign Up"} />
+          <SubHeading label={"Enter your details to create your account"} />
           <InputBox placeholder="hello@kapoor.com" label={"Email"} />
           <InputBox placeholder="123456" label={"Password"} />
+          <InputBox placeholder="Steve Jobs" label={"Name"} />
+          <InputBox placeholder="UP32XXXX" label={"Vehicle No"} />
+          <InputBox placeholder="FOPPXXXXX" label={"Driving License"} />
           <div className="pt-4">
-            <Button label={"Sign in"} />
+            <Button label={"Sign Up"} />
           </div>
-          <BottomWarning
-            label={"Don't have an account?"}
-            buttonText={"Sign up"}
-            to={"/signup"}
-          />
         </div>
       </div>
     </div>
